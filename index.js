e43a15f7e0dcce0e689f32b2397763c4bcc91f6d
JRC <jrcelebrados@gmail.com>
2021-04-30 18:53:06 -0400

    Fixed mult and div buttons

--- index.js
+++ index.js
@@ -1,56 +1,38 @@
 var textArea = document.getElementById('textArea');
 
-// function pushBtn(obj) {
-//   var pushed = obj.innerHTML;
-//   textArea.innerHTML += pushed;
-// }
-//
-// let oneBtnJS = document.getElementById("oneButton");
-// oneBtnJS.addEventListener("click", function() { pushBtn('1'); });
-//
-// let twoBtnJS = document.getElementById("twoButton");
-// twoBtnJS.addEventListener("click", function() { pushBtn('2'); });
-//
-// let threeBtnJS = document.getElementById("threeButton");
-// threeBtnJS.addEventListener("click", function() { pushBtn('3'); });
-//
-// let fourBtnJS = document.getElementById("fourButton");
-// fourBtnJS.addEventListener("click", function() { pushBtn('4'); });
-//
-// let fiveBtnJS = document.getElementById("fiveButton");
-// fiveBtnJS.addEventListener("click", function() { pushBtn('5'); });
-//
-// let sixBtnJS = document.getElementById("sixButton");
-// sixBtnJS.addEventListener("click", function() { pushBtn('6'); });
-//
-// let sevenBtnJS = document.getElementById("sevenButton");
-// sevenBtnJS.addEventListener("click", function() { pushBtn('7'); });
-//
-// let eightBtnJS = document.getElementById("eightButton");
-// eightBtnJS.addEventListener("click", function() { pushBtn('8'); });
-//
-// let nineBtnJS = document.getElementById("nineButton");
-// nineBtnJS.addEventListener("click", function() { pushBtn('9'); });
-
-
 function pushBtn(obj) {
 
   var pushed = obj.innerHTML;
 
-    if (pushed == '=') {
-      // Calculate
-      textArea.innerHTML = eval(textArea.innerHTML);
+  if (pushed == '=') {
+    // Calculate
+    textArea.innerHTML = eval(textArea.innerHTML);
 
-      } else if (pushed == 'C') {
-        // All Clear
-        textArea.innerHTML = '0';
+  } else if (pushed == 'C') {
+    // All Clear
+    textArea.innerHTML = '0';
 
-      } else {
-        if (textArea.innerHTML == '0') {
+  } else {
+      if (textArea.innerHTML == '0') {
         textArea.innerHTML = pushed;
 
       } else {
         textArea.innerHTML += pushed;
     }
   }
+
+}
+
+function pushMultBtn(obj) {
+
+  var pushed = document.getElementById("multiplyButton").value;
+  textArea.innerHTML += pushed;
+
+}
+
+function pushDivideBtn(obj) {
+
+  var pushed = document.getElementById("divideButton").value;
+  textArea.innerHTML += pushed;
+
 }
